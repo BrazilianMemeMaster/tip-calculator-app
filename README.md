457d1701b627934e845bc10a774b46c42da16bbb
BrazilianMemeMaster <aalexandrexd@hotmail.com>
2022-03-18 16:00:42 -0300

    finished challenge

---
+++ README.md
@@ -0,0 +1,46 @@
+# Frontend Mentor - Tip calculator app solution
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+![](./images/screenshot.jpg)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://github.com/BrazilianMemeMaster/tip-calculator-app)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+
+## Author
+
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/BrazilianMemeMaster)
+- Twitter - [@yourusername](https://www.twitter.com/mememaster_xd)
---
+++ app.js
@@ -0,0 +1,118 @@
+const tips = document.querySelectorAll('.tip-option');
+const resetBtn = document.querySelector('#reset');
+const billInput = document.querySelector('#bill-input');
+const billInputGroup = document.querySelector('.bill-input-group');
+const customTip = document.querySelector('.custom');
+const peopleNum = document.querySelector('#people-input');
+const peopleInputGroup = document.querySelector('.people-input-group');
+
+let totalResult = document.querySelector('#total-result').lastChild;
+let tipResult = document.querySelector('#tip-result').lastChild;
+
+let currentTip;
+let currentBill;
+let currentPeople;
+
+//functions
+
+const removeSelected = () => {
+    customTip.value = '';
+    let selected = document.querySelector('.selected');
+    if (selected) {
+        selected.classList.remove('selected');
+    }
+}
+
+const clear = () => {
+    totalResult.innerText = '0.00'
+    tipResult.innerText = '0.00'
+    billInput.value = '';
+    peopleNum.value = '';
+    let selected = document.querySelector('.selected');
+    if (selected) {
+        selected.classList.remove('selected');
+    }
+}
+
+const addSelected = (b) => {
+    b.classList.add('selected');
+}
+
+const calculateTotalResult = () => {
+    let totalRes = (currentBill / currentPeople) + ((currentBill * (currentTip / 100)) / currentPeople);
+    return Math.floor(totalRes * 100) / 100;
+}
+
+const calculateTipResult = () => {
+    let tipRes = (currentBill * (currentTip / 100)) / currentPeople;
+    return Math.floor(tipRes * 100) / 100
+
+}
+
+const showResults = () => {
+    if (calculateTipResult() === Infinity || calculateTipResult() === "" || isNaN(calculateTipResult())) {
+        totalResult.innerText = '0.00'
+        tipResult.innerText = '0.00'
+    } else {
+        totalResult.innerText = calculateTotalResult();
+        tipResult.innerText = calculateTipResult();
+    }
+}
+
+
+//Event Listeners
+
+billInput.addEventListener('focus', () => {
+    billInputGroup.classList.add('active');
+})
+billInput.addEventListener('blur', () => {
+    billInputGroup.classList.remove('active');
+})
+
+billInput.addEventListener('keyup', () => {
+    currentBill = billInput.value
+    showResults();
+})
+
+
+customTip.addEventListener('focus', () => {
+    customTip.classList.add('active');
+})
+customTip.addEventListener('blur', () => {
+    customTip.classList.remove('active');
+})
+
+peopleNum.addEventListener('focus', () => {
+    peopleInputGroup.classList.add('active');
+})
+peopleNum.addEventListener('blur', () => {
+    peopleInputGroup.classList.remove('active');
+})
+
+peopleNum.addEventListener('keyup', () => {
+    currentPeople = peopleNum.value;
+    showResults();
+})
+
+customTip.addEventListener('click', () => {
+    removeSelected();
+})
+
+customTip.addEventListener('keyup', () => {
+    currentTip = customTip.value;
+    showResults();
+})
+
+tips.forEach(btn => {
+    btn.addEventListener('click', () => {
+        removeSelected();
+        addSelected(btn);
+        let value = btn.id;
+        currentTip = value;
+        showResults();
+    })
+})
+
+resetBtn.addEventListener('click', () => {
+    window.location.reload(true)
+})
